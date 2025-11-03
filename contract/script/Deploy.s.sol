d7dd5b44f781b91966abe11333e036c9547856c1
obah <olusojiobah@gmail.com>
2025-11-03 12:34:10 +0100

    chore: deployed to base sepolia

--- contract/script/Deploy.s.sol
+++ contract/script/Deploy.s.sol
@@ -5,7 +5,10 @@ import {Script} from "forge-std/Script.sol";
 import {MiniArtWork} from "../src/MiniArtwork.sol";
 
 contract Deploy is Script {
+    // uint constant BASE_SEPOLIA_CHAIN_ID = 84532;
+
     function run() external {
+        // vm.selectFork(BASE_SEPOLIA_CHAIN_ID);
         vm.startBroadcast();
         new MiniArtWork();
         vm.stopBroadcast();
